73f29d42ba08f5e1809a4ad536f88f845a99e551
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-11 20:46:46 -0300

    Initial file setup.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import wheresWaldoImg from '../images/wheres-waldo.jpg';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={wheresWaldoImg} />
+    </div>
+  );
+}
+
+export default App;
