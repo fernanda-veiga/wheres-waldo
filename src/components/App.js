4a793f1f84f9e021f9a5f14e6afeb7a148bd567d
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-12 15:51:52 -0300

    Added divs to visualize the character's positions on the screen. Added a function to be called whenever the user clicks anywhere in the image. This function gets the current image height and width and the position where the user clicked. It calculates the Wizard's position and checks if the click was in that position.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import wheresWaldoImg from '../images/wheres-waldo.jpg';
-import '../styles/App.css';
+import React from "react";
+import wheresWaldoImg from "../images/wheres-waldo.jpg";
+import "../styles/App.css";
 
 function App() {
+  function handleClick(event) {
+    const IMG_WIDTH = event.target.width;
+    const IMG_HEIGHT = event.target.height;
+    const CLICK_X = event.pageX;
+    const CLICK_Y = event.pageY;
+
+    //Find Wizard
+    if (
+      CLICK_X >= IMG_WIDTH * 0.0583 &&
+      CLICK_X <= IMG_WIDTH * 0.0907 &&
+      CLICK_Y >= IMG_HEIGHT * 0.7318 &&
+      CLICK_Y <= IMG_HEIGHT * 0.7773
+    ) {
+      console.log("Wizard is here");
+    }
+  }
+
+  function windowResize() {
+    console.log("window resized");
+  }
+  window.onresize = windowResize;
+
   return (
     <div className="App">
-        <img src={wheresWaldoImg} />
+      <div className="Odlaw"></div>
+      <div className="Wizard"></div>
+      <div className="Waldo"></div>
+      <div className="Wanda"></div>
+      <img src={wheresWaldoImg} onClick={handleClick} />
     </div>
   );
 }
