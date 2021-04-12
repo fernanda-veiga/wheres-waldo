85cd715a4226ab57c7d1decdeef77b3ce8328dec
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-12 19:15:15 -0300

    Added the relative position of each character to a firestore database. Read the data in the handleClick function and added, for each character, a conditional that checks if the position clicked by user is inside the character's position range stored in firestore.

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,76 @@ import React from "react";
 import wheresWaldoImg from "../images/wheres-waldo.jpg";
 import "../styles/App.css";
 
-//Import Firebase products
+//Initialize Firebase products
 import firebase from "firebase/app";
 import "firebase/firestore";
 
+let firebaseConfig = {
+  apiKey: "AIzaSyDoZ82DZFbGsMptB51tWfd3t0wx5g1qiJw",
+  authDomain: "whereswaldo-5f140.firebaseapp.com",
+  projectId: "whereswaldo-5f140",
+  storageBucket: "whereswaldo-5f140.appspot.com",
+  messagingSenderId: "795910774501",
+  appId: "1:795910774501:web:699def5b7b8224e4857e0c",
+  measurementId: "G-YPJ5GZKGKJ",
+};
+firebase.initializeApp(firebaseConfig);
+
+//
+
+let charactersDatabasePath = firebase
+  .firestore()
+  .collection("characters")
+  .doc("4io2HrKnIaopfg1C8XMQ");
+
 function App() {
   function handleClick(event) {
-    const IMG_WIDTH = event.target.width;
-    const IMG_HEIGHT = event.target.height;
-    const CLICK_X = event.pageX;
-    const CLICK_Y = event.pageY;
+    const imgWidth = event.target.width;
+    const imgHeight = event.target.height;
+    const clickX = event.pageX;
+    const clickY = event.pageY;
+    let characters;
 
-    //Find Wizard
-    if (
-      CLICK_X >= IMG_WIDTH * 0.0583 &&
-      CLICK_X <= IMG_WIDTH * 0.0907 &&
-      CLICK_Y >= IMG_HEIGHT * 0.7318 &&
-      CLICK_Y <= IMG_HEIGHT * 0.7773
-    ) {
-      console.log("Wizard is here");
-    }
+    charactersDatabasePath.get().then((doc) => {
+      characters = doc.data();
+
+      //Find Wizard
+      if (
+        clickX >= imgWidth * characters.wizard.xRelativeInitial &&
+        clickX <= imgWidth * characters.wizard.xRelativeFinal &&
+        clickY >= imgHeight * characters.wizard.yRelativeInitial &&
+        clickY <= imgHeight * characters.wizard.yRelativeFinal
+      ) {
+        console.log("Wizard is here");
+      }
+      //Find Odlaw
+      else if (
+        clickX >= imgWidth * characters.odlaw.xRelativeInitial &&
+        clickX <= imgWidth * characters.odlaw.xRelativeFinal &&
+        clickY >= imgHeight * characters.odlaw.yRelativeInitial &&
+        clickY <= imgHeight * characters.odlaw.yRelativeFinal
+      ) {
+        console.log("Odlaw is here");
+      }
+      //Find Wenda
+      else if (
+        clickX >= imgWidth * characters.wenda.xRelativeInitial &&
+        clickX <= imgWidth * characters.wenda.xRelativeFinal &&
+        clickY >= imgHeight * characters.wenda.yRelativeInitial &&
+        clickY <= imgHeight * characters.wenda.yRelativeFinal
+      ) {
+        console.log("Wenda is here");
+      }
+      //Find Waldo
+      else if (
+        clickX >= imgWidth * characters.waldo.xRelativeInitial &&
+        clickX <= imgWidth * characters.waldo.xRelativeFinal &&
+        clickY >= imgHeight * characters.waldo.yRelativeInitial &&
+        clickY <= imgHeight * characters.waldo.yRelativeFinal
+      ) {
+        console.log("Waldo is here");
+      }
+    });
   }
 
   function windowResize() {
@@ -35,7 +85,7 @@ function App() {
       <div className="Wizard"></div>
       <div className="Waldo"></div>
       <div className="Wanda"></div>
-      <img src={wheresWaldoImg} onClick={handleClick} />
+      <img src={wheresWaldoImg} onClick={handleClick} alt="" />
     </div>
   );
 }
