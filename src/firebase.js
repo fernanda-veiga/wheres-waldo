f4b7e2ebff776af283c563a4ddbc04356732171c
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-14 16:02:53 -0300

    Moved the firebase configuration to its own module.

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+let firebaseConfig = {
+  apiKey: "AIzaSyDoZ82DZFbGsMptB51tWfd3t0wx5g1qiJw",
+  authDomain: "whereswaldo-5f140.firebaseapp.com",
+  projectId: "whereswaldo-5f140",
+  storageBucket: "whereswaldo-5f140.appspot.com",
+  messagingSenderId: "795910774501",
+  appId: "1:795910774501:web:699def5b7b8224e4857e0c",
+  measurementId: "G-YPJ5GZKGKJ",
+};
+
+export default firebaseConfig;
