fcb7e9b3f0b87c8545f5acf31af9e787a6585cc0
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-19 18:16:32 -0300

    Modified the Header to show only the logo and a button to switch between the Game page and the Leaderboard page.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,7 @@
 import React from "react";
 import "../styles/Header.css";
 import waldoLogo from "../images/waldo-logo.png";
-import odlaw from "../images/odlaw.png";
-import waldo from "../images/waldo.png";
-import wenda from "../images/wenda.png";
-import wizard from "../images/wizard.png";
-import secondsToTimeStr from "../utility/timer";
-
-function Character(props) {
-  return (
-    <div className="Header-right-character">
-      <img
-        className={"Header-" + props.name + "-img"}
-        src={props.img}
-        alt={props.name + " avatar"}
-      />
-      <p className={"Header-" + props.name + "-name"}>
-        {props.name.charAt(0).toUpperCase() + props.name.slice(1)}
-      </p>
-    </div>
-  );
-}
+import { Link } from "react-router-dom";
 
 function Header(props) {
   return (
@@ -29,23 +10,17 @@ function Header(props) {
         <img src={waldoLogo} alt="Waldo" />
         <h1>Where's Waldo</h1>
       </div>
-      {props.type === "homepage" ? (
-        <div className="Header-right">
-          <button type="button" className="Header-leaderboard">
+      <div className="Header-right">
+        {props.type === "game" ? (
+          <Link className="Header-right-link" to="/leaderboard">
             Leaderboard
-          </button>
-        </div>
-      ) : (
-        <div className="Header-right">
-          <Character img={odlaw} name={"odlaw"} />
-          <Character img={waldo} name={"waldo"} />
-          <Character img={wenda} name={"wenda"} />
-          <Character img={wizard} name={"wizard"} />
-          <div className="Header-right-timer">
-            {secondsToTimeStr(props.time)}
-          </div>
-        </div>
-      )}
+          </Link>
+        ) : (
+          <Link className="Header-right-link" to="/">
+            Play
+          </Link>
+        )}
+      </div>
     </header>
   );
 }
