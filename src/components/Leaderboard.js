8fc5eef75791accaff2df3f86b7540a411f8dc7b
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-19 18:17:34 -0300

    Added a Leaderboard component to show the players ranking.

---
+++ src/components/Leaderboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import "../styles/Leaderboard.css";
+
+function Leaderboard() {
+  return (
+    <div className="Leaderboard">
+      <Header type="leaderboard" />
+    </div>
+  );
+}
+
+export default Leaderboard;
