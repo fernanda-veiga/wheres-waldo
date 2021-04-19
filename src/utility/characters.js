1e99a924ef8ac9283f44b5e4337d85e2b224d341
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-19 18:20:32 -0300

    Imported the characters images to the chacters object.

--- src/utility/characters.js
+++ src/utility/characters.js
@@ -1,9 +1,14 @@
+import odlaw from "../images/odlaw.png";
+import waldo from "../images/waldo.png";
+import wenda from "../images/wenda.png";
+import wizard from "../images/wizard.png";
+
 function createCharacters() {
   return {
-    odlaw: { found: false },
-    waldo: { found: false },
-    wenda: { found: false },
-    wizard: { found: false },
+    odlaw: { img: odlaw, found: false },
+    waldo: { img: waldo, found: false },
+    wenda: { img: wenda, found: false },
+    wizard: { img: wizard, found: false },
   };
 }
 
