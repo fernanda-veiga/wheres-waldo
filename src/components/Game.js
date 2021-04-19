31d9b33a8c653c1c9245ec5f19acca9d09b3fda0
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-19 19:01:48 -0300

    Changed the Characters component to show only the images and fixed the styling to fit the changes.

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,23 +7,19 @@ import createCharacters, { charactersNames } from "../utility/characters";
 //import secondsToTimeStr from "../utility/timer";
 //import { Link } from "react-router-dom";
 
-function Character(props) {
+function Characters() {
   const characters = createCharacters();
 
   return (
     <div className="Game-info-character">
-      <img
-        className={"Game-info-character-img"}
-        id={"Game-info-character-img-" + props.character}
-        src={characters[props.character].img}
-        alt={props.character + " avatar"}
-      />
-      {/*<p
-        className={"Game-info-character-text"}
-        id={"Game-info-character-text-" + props.character}
-      >
-        {props.character.charAt(0).toUpperCase() + props.character.slice(1)}
-      </p>*/}
+      {charactersNames.map((character) => (
+        <img
+          className="Game-info-character-img"
+          id={"Game-info-character-img-" + character}
+          src={characters[character].img}
+          alt={character + " avatar"}
+        />
+      ))}
     </div>
   );
 }
@@ -34,9 +30,7 @@ function Game() {
       <Header type="game" />
       <div className="Game-content">
         <div className="Game-info">
-          {charactersNames.map((character) => (
-            <Character character={character} />
-          ))}
+          <Characters />
         </div>
         <img src={wheresWaldoImg} /*onClick={handleImgClick}*/ alt="" />
       </div>
