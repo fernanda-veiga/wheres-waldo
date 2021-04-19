bc6e95ef511c9c9896172f03c7ce725727a957af
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-19 18:51:17 -0300

    Added a bar below the Header to show the characters hidden in the image.

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,56 +2,44 @@ import React from "react";
 import Header from "./Header";
 import "../styles/Game.css";
 import wheresWaldoImg from "../images/wheres-waldo.jpg";
+import createCharacters, { charactersNames } from "../utility/characters";
 
-//import "../styles/Header.css";
-//import odlaw from "../images/odlaw.png";
-//import waldo from "../images/waldo.png";
-//import wenda from "../images/wenda.png";
-//import wizard from "../images/wizard.png";
 //import secondsToTimeStr from "../utility/timer";
 //import { Link } from "react-router-dom";
 
-/*function Character(props) {
+function Character(props) {
+  const characters = createCharacters();
+
   return (
-    <div className="Header-right-character">
+    <div className="Game-info-character">
       <img
-        className={"Header-" + props.name + "-img"}
-        src={props.img}
-        alt={props.name + " avatar"}
+        className={"Game-info-character-img"}
+        id={"Game-info-character-img-" + props.character}
+        src={characters[props.character].img}
+        alt={props.character + " avatar"}
       />
-      <p className={"Header-" + props.name + "-name"}>
-        {props.name.charAt(0).toUpperCase() + props.name.slice(1)}
-      </p>
+      {/*<p
+        className={"Game-info-character-text"}
+        id={"Game-info-character-text-" + props.character}
+      >
+        {props.character.charAt(0).toUpperCase() + props.character.slice(1)}
+      </p>*/}
     </div>
   );
 }
-
-function Header(props) {
-  return (
-    <header className="Header">
-      <div className="Header-left">
-        <img src={waldoLogo} alt="Waldo" />
-        <h1>Where's Waldo</h1>
-      </div>
-      <div className="Header-right">
-        <Character img={odlaw} name={"odlaw"} />
-        <Character img={waldo} name={"waldo"} />
-        <Character img={wenda} name={"wenda"} />
-        <Character img={wizard} name={"wizard"} />
-        <div className="Header-right-timer">
-          00:00
-          {/*secondsToTimeStr(props.time)*
-        </div>
-      </div>
-    </header>
-  );
-}*/
 
 function Game() {
   return (
     <div className="Game">
       <Header type="game" />
-      <img src={wheresWaldoImg} /*onClick={handleImgClick}*/ alt="" />
+      <div className="Game-content">
+        <div className="Game-info">
+          {charactersNames.map((character) => (
+            <Character character={character} />
+          ))}
+        </div>
+        <img src={wheresWaldoImg} /*onClick={handleImgClick}*/ alt="" />
+      </div>
     </div>
   );
 }
