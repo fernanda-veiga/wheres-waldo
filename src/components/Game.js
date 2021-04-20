88cb0a8c607458f36194a7d2a63fcbfa65e93ab6
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-20 10:03:36 -0300

    Added a function to check if the character clicked is correct. If it's correct, the function disables the clicked buttons, changed the found status of the character, changes the display of some elements and checks if all characters are found.

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,7 +8,8 @@ import Highlight from "./game-components/Highlight";
 
 //Utility
 import createCharacters, { charactersNames } from "../utility/characters";
-import showHighlight from "../utility/highlight";
+import { showHighlight, changeElementDisplay } from "../utility/dom";
+import { getCharacters } from "../firebase";
 
 //Images
 import wheresWaldoImg from "../images/wheres-waldo.jpg";
@@ -18,10 +19,15 @@ import "../styles/Game.css";
 
 //import secondsToTimeStr from "../utility/timer";
 
+const characters = createCharacters();
+
 function Game() {
   const [click, setClick] = useState({ x: 0, y: 0 });
   const [imgSize, setImgSize] = useState({ w: 0, h: 0 });
   const [startTime, setStartTime] = useState({});
+  const [endTime, setEndTime] = useState({});
+
+  //console.log(characters);
 
   function startGame() {
     document.querySelector(".Game-popup-container").style.display = "none";
@@ -34,11 +40,45 @@ function Game() {
     showHighlight({ x: event.pageX, y: event.pageY });
   }
 
+  function checkCharacter(event) {
+    const character = event.target.id.slice(14);
+
+    getCharacters(character).then((doc) => {
+      const characterData = doc.data();
+      const HEADER_HEIGHT = 200;
+
+      if (
+        click.x >= imgSize.w * characterData.x[0] &&
+        click.x <= imgSize.w * characterData.x[1] &&
+        click.y >= imgSize.h * characterData.y[0] + HEADER_HEIGHT &&
+        click.y <= imgSize.h * characterData.y[1] + HEADER_HEIGHT
+      ) {
+        console.log("character found");
+        event.target.disabled = true;
+        characterFound(character);
+        changeElementDisplay(character);
+        allCharactersFound();
+      }
+    });
+  }
+
+  function characterFound(character) {
+    characters[character].found = true;
+  }
+
+  function allCharactersFound() {
+    if (
+      charactersNames.every((character) => characters[character].found === true)
+    ) {
+      console.log("All characters found");
+    }
+  }
+
   return (
     <div className="Game">
       <Header type="game" />
       <div className="Game-content">
-        <Highlight />
+        <Highlight checkCharacter={checkCharacter} />
         <div className="Game-info">
           <Characters />
         </div>
