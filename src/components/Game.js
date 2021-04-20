74fbabbda1ddbf0878a0ef39b16d7d622dd72590
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-19 22:29:03 -0300

    Moved the Highlight component to another folder and added the component to the Game component. Fixed the styling to fit the changes.

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,79 +2,52 @@ import React, { useState } from "react";
 
 //Components
 import Header from "./Header";
+import Characters from "./game-components/Characters";
+import Popup from "./game-components/Popup";
+import Highlight from "./game-components/Highlight";
 
 //Utility
 import createCharacters, { charactersNames } from "../utility/characters";
+import showHighlight from "../utility/highlight";
 
 //Images
 import wheresWaldoImg from "../images/wheres-waldo.jpg";
 
 //CSS
 import "../styles/Game.css";
-import "../styles/Popup.css";
 
 //import secondsToTimeStr from "../utility/timer";
 
-function Characters() {
-  const characters = createCharacters();
-
-  return (
-    <div className="Game-info-character">
-      {charactersNames.map((character) => (
-        <img
-          className="Game-info-character-img"
-          id={"Game-info-character-img-" + character}
-          src={characters[character].img}
-          alt={character + " avatar"}
-        />
-      ))}
-    </div>
-  );
-}
-
-function Popup() {
-  const characters = createCharacters();
-
-  return (
-    <div className="Game-popup-container">
-      <div className="Game-popup">
-        <img className="Game-popup-img" src={wheresWaldoImg} alt="" />
-        <div className="Game-popup-info">
-          <p>Find the following characters:</p>
-          <div className="Game-popup-info-characters">
-            {charactersNames.map((character) => (
-              <div className="Game-popup-info-character">
-                <img
-                  src={characters[character].img}
-                  alt={character + " avatar"}
-                />
-                <p>{character.charAt(0).toUpperCase() + character.slice(1)}</p>
-              </div>
-            ))}
-          </div>
-          <button>START</button>
-        </div>
-      </div>
-    </div>
-  );
-}
-
 function Game() {
-  //const [click, setClick] = useState({ x: 0, y: 0 });
-  //const [imgSize, setImgSize] = useState({ w: 0, h: 0 });
+  const [click, setClick] = useState({ x: 0, y: 0 });
+  const [imgSize, setImgSize] = useState({ w: 0, h: 0 });
+  const [startTime, setStartTime] = useState({});
+
+  function startGame() {
+    document.querySelector(".Game-popup-container").style.display = "none";
+    setStartTime(new Date());
+  }
+
+  function handleImgClick(event) {
+    setClick({ x: event.pageX, y: event.pageY });
+    setImgSize({ w: event.target.offsetWidth, h: event.target.offsetHeight });
+    showHighlight({ x: event.pageX, y: event.pageY });
+  }
 
   return (
     <div className="Game">
       <Header type="game" />
       <div className="Game-content">
+        <Highlight />
         <div className="Game-info">
           <Characters />
         </div>
-        <Popup />
+        <Popup startGame={startGame} />
         <img
           className="Game-img"
           src={wheresWaldoImg}
-          /*onClick={handleImgClick}*/ alt=""
+          onClick={handleImgClick}
+          alt=""
         />
       </div>
     </div>
